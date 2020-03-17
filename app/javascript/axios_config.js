161db3f5df7efdfd67dc14c4bfafd0f95e190106
Kazuto Sato <kazubonx@gmail.com>
2020-03-17 14:55:02 +0900

    IE11対策変更

--- app/javascript/axios_config.js
+++ app/javascript/axios_config.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 Axios.interceptors.request.use((config) => {
-  if(['post', 'put', 'patch', 'delete'].includes(config.method)) {
+  if(config.method == 'post' || config.method == 'put' ||
+     config.method == 'patch' || config.method == 'delete') {
     config.headers['X-CSRF-Token'] = $('meta[name="csrf-token"]').attr('content');
   }
   return config;
